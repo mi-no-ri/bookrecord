ee5ff81fe268508e4ead6387de19f4125cd671d5
実 <harada.miyu.0901@gmail.com>
2026-07-13 23:12:27 +0900

    ルーティング設定の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get "books/index"
-  get "books/show"
-  get "books/new"
-  get "books/create"
-  get "books/edit"
-  get "books/update"
-  get "books/destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -18,4 +11,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # 一覧画面をトップページとして表示
+  root "books#index"
+  # BooksControllerのルーティング
+  resources :books
 end
