33a2cd53d14e3be6cce7f96c21dcb3cb88a7712a
実 <harada.miyu.0901@gmail.com>
2026-07-22 18:01:18 +0900

    削除機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,40 +4,53 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+  # 詳細画面
   def show
     @book = Book.find(params[:id])
   end
 
+  # 新規登録画面
   def new
     @book = Book.new
   end
 
+  # 書籍を登録
   def create
     @book = Book.new(book_params)
 
     if @book.save
+      # 登録成功時は詳細画面へ移行
       redirect_to @book, notice: "書籍を登録しました"
     else
+      # 失敗時は登録画面を再表示
       render :new, status: :unprocessable_entity
     end
   end
 
+  # 編集画面
   def edit
     @book = Book.find(params[:id])
   end
-
+  # 書籍情報を更新
   def update
-    @book = Book.find(parms[:id])
+    @book = Book.find(params[:id])
 
-    if @plan.update
+    if @book.update(book_params)
+      # 更新成功時は詳細画面へ移行
       redirect_to @book, notice: "更新しました"
     else
-      render :new, status: :unprocessable_entity
-
+      # 失敗時は編集画面を再表示
+      render :edit, status: :unprocessable_entity
     end
   end
 
+  # 書籍を削除
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+
+    # 削除後、一覧画面へ移行
+    redirect_to books_path, notice: "削除しました", status: :see_other
   end
 
   private
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
-  # 読書状況の選択肢を定義
+  # 読書状況の選択肢(enum)を定義
   enum reading_status: { unread: 0, reading: 1, finished: 2 }
-  # 入手方法の選択肢を定義
+  # 入手方法の選択肢(enum)を定義
   enum acquisition_type: { not_owned: 0, purchase: 1, borrowed: 2 }
 end
