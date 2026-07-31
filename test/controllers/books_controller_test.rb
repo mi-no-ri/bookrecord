2bbddbb51d8a7d2919f1e844799964ded250330d
実 <harada.miyu.0901@gmail.com>
2026-07-31 16:24:35 +0900

    書籍管理機能のコントローラーテストを修正

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,38 +1,65 @@
 require "test_helper"
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
+  setup do
+   @user = users(:one)
+
+    # ログイン
+    post login_path, params: {
+      email: @user.email,
+      password: "password"
+    }
+
+
+    @book = books(:one)
+  end
+
   test "should get index" do
-    get books_index_url
+    get books_url
     assert_response :success
   end
 
   test "should get show" do
-    get books_show_url
+    get book_url(@book)
     assert_response :success
   end
 
   test "should get new" do
-    get books_new_url
+    get new_book_url
     assert_response :success
   end
 
-  test "should get create" do
-    get books_create_url
-    assert_response :success
-  end
+  test "should create book" do
+    assert_difference("Book.count") do
+      post books_url, params: {
+        book: {
+          title: "テスト本",
+          author: "テスト著者",
+          started_on: Date.today,
+          finished_on: Date.today,
+          reading_status: :reading,
+          acquisition_type: :purchase,
+          paper: true,
+          ebook: false,
+          memo: "テスト"
+        }
+      }
+    end
 
+    assert_redirected_to book_path(Book.last)
+  end
   test "should get edit" do
-    get books_edit_url
+    get edit_book_url(@book)
     assert_response :success
   end
 
   test "should get update" do
-    get books_update_url
+    get book_url(@book)
     assert_response :success
   end
 
   test "should get destroy" do
-    get books_destroy_url
+    get book_url(@book)
     assert_response :success
   end
 end
